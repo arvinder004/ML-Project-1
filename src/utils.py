572b4cc6713bf34e04df0739fdcbc2981d6e861c
arvinder004 <asdhoul004@gmail.com>
2025-12-01 16:42:00 +0530

    Tuned Hyperparameters

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,7 @@ import sys
 import pandas as pd
 import numpy as np
 import dill
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import r2_score
 
 from src.exception import CustomeException
@@ -22,14 +22,21 @@ def save_object(file_path, obj):
         raise CustomeException(e, sys)
     
 
-def evaluate_models(x_train, y_train, x_test, y_test, models):
+def evaluate_models(x_train, y_train, x_test, y_test, models, params):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = params[list(models.keys())[i]]
 
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(x_train, y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(x_train, y_train)
+
+            # model.fit(x_train, y_train)
 
             y_train_pred = model.predict(x_train)
             y_test_pred = model.predict(x_test)
